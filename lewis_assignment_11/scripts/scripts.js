c153cad56b09ab89436cd59c07200cfb54894b1c
Richard Lewis <97256133+Tirialoxliem@users.noreply.github.com>
2023-06-06 17:43:03 -0400

    Add files via upload

---
+++ lewis_assignment_11/scripts/scripts.js
@@ -0,0 +1,250 @@
+function myFunction2a() {
+  alert("The oldest choral repertoire was sung in unison and carved in stone!");
+}
+
+function myFunction2b() {
+  alert("The largest orchestra in the world has over 12,000 members!");
+}
+
+function myFunction2c() {
+  alert(
+    "A poll taken in 2018 shows that country music is the most popular music in the United States, with 21% of americans voting for it!"
+  );
+}
+
+function myFunction2d() {
+  alert("Easy listening music has been around for almost a century!");
+}
+
+function myFunction2f() {
+  alert("As of 2015 heavy metal music was the most popular music on Spotify!");
+}
+
+function myFunction2g() {
+  alert("Hip hop was born in 1973 in the Bronx at a birthday party!");
+}
+
+function myFunction2h() {
+  alert("Jazz music started out as pop music in the 1920's!");
+}
+
+function myFunction2i() {
+  alert(
+    "The Beach Boys next number one song after Good Vibrations was Kokomo!"
+  );
+}
+
+function myFunction2j() {
+  alert(
+    "The term 'pop music' became commonly used in the 1950's when rock and roll became a 'popular' music to the teens!"
+  );
+}
+
+function myFunction2k() {
+  alert(
+    "The term rhythm and blues was a product of the post-World War II music industry's effort to find a new word to replace the category that had been known for several decades as 'race records'!"
+  );
+}
+
+function myFunction2l() {
+  alert("Elvis' manager forbade him from touring internationally!");
+}
+
+var a = document.getElementById("myAudio3a");
+var b = document.getElementById("myAudio3b");
+var c = document.getElementById("myAudio3c");
+var d = document.getElementById("myAudio3d");
+var e = document.getElementById("myAudio3e");
+var f = document.getElementById("myAudio3f");
+var g = document.getElementById("myAudio3g");
+var h = document.getElementById("myAudio3h");
+var i = document.getElementById("myAudio3i");
+var j = document.getElementById("myAudio3j");
+var k = document.getElementById("myAudio3k");
+var l = document.getElementById("myAudio3l");
+var m = document.getElementById("myAudio3m");
+var n = document.getElementById("myAudio3n");
+var o = document.getElementById("myAudio3o");
+var p = document.getElementById("myAudio3p");
+var q = document.getElementById("myAudio3q");
+var r = document.getElementById("myAudio3r");
+
+function myFunction3a() {
+  if (a.paused) {
+    a.play();
+    btn.innerHTML = "Pause";
+  } else {
+    a.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3b() {
+  if (b.paused) {
+    b.play();
+    btn.innerHTML = "Pause";
+  } else {
+    b.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3c() {
+  if (c.paused) {
+    c.play();
+    btn.innerHTML = "Pause";
+  } else {
+    c.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3d() {
+  if (d.paused) {
+    d.play();
+    btn.innerHTML = "Pause";
+  } else {
+    d.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3e() {
+  if (e.paused) {
+    e.play();
+    btn.innerHTML = "Pause";
+  } else {
+    e.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3f() {
+  if (f.paused) {
+    f.play();
+    btn.innerHTML = "Pause";
+  } else {
+    f.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3g() {
+  if (g.paused) {
+    g.play();
+    btn.innerHTML = "Pause";
+  } else {
+    g.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3h() {
+  if (h.paused) {
+    h.play();
+    btn.innerHTML = "Pause";
+  } else {
+    h.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3i() {
+  if (i.paused) {
+    i.play();
+    btn.innerHTML = "Pause";
+  } else {
+    i.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3j() {
+  if (j.paused) {
+    j.play();
+    btn.innerHTML = "Pause";
+  } else {
+    j.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3k() {
+  if (k.paused) {
+    k.play();
+    btn.innerHTML = "Pause";
+  } else {
+    k.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3l() {
+  if (l.paused) {
+    l.play();
+    btn.innerHTML = "Pause";
+  } else {
+    l.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3m() {
+  if (m.paused) {
+    m.play();
+    btn.innerHTML = "Pause";
+  } else {
+    m.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3n() {
+  if (n.paused) {
+    n.play();
+    btn.innerHTML = "Pause";
+  } else {
+    n.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3o() {
+  if (o.paused) {
+    o.play();
+    btn.innerHTML = "Pause";
+  } else {
+    o.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3p() {
+  if (p.paused) {
+    p.play();
+    btn.innerHTML = "Pause";
+  } else {
+    p.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3q() {
+  if (q.paused) {
+    q.play();
+    btn.innerHTML = "Pause";
+  } else {
+    q.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function myFunction3r() {
+  if (r.paused) {
+    r.play();
+    btn.innerHTML = "Pause";
+  } else {
+    r.pause();
+    btn.innerHTML = "Play";
+  }
+}
